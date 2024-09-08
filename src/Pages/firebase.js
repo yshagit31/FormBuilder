bc58104003333c9d96578243eba3b8a62900f3f1
yshagit31 <yshashank024@gmail.com>
2024-09-08 23:59:23 +0530

    Hide

--- src/Pages/firebase.js
+++ src/Pages/firebase.js
@@ -1,21 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getFirestore} from 'firebase/firestore';
-
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCeF6sSUy-29gR2kRyCFimLNY1XGEI1Cf8",
-  authDomain: "feedbackform-81922.firebaseapp.com",
-  projectId: "feedbackform-81922",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: "feedbackform-81922.appspot.com",
   messagingSenderId: "165533688393",
   appId: "1:165533688393:web:e78ba9f4405afe52cf1598",
   measurementId: "G-SB1Q6DDE27"
 };
 
-
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const db=getFirestore(app);
+const db = getFirestore(app);
 
 export default db;
